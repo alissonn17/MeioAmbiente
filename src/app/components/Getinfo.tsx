027a3dba86bd7048227b4780b20f79cf65f2c722
Alisson <llielzinh@gmail.com>
2025-06-26 20:52:21 -0300

    :sparkles: feat: add getinfo component

--- src/app/components/Getinfo.tsx
+++ src/app/components/Getinfo.tsx
@@ -1,39 +1,55 @@
 "use client"
 import { JSX, useEffect, useState } from "react"
 
-interface resprop{
-    info: string;
+interface resprop {
+  Main: {
+    Importancia: string;
+    Dever: string;
+  };
 }
 
-export default function GetInfo(): JSX.Element{
-    const [res, setRes]=useState<resprop[]>([]);
+/**
+ * Get info from main in API-Meio Ambiente.
+ */
 
-    useEffect(()=>{async function fetchMain(){
-        try{
-            const response = await fetch("https://apimeioambiente.onrender.com");
-            if (!response.ok){
-                throw new Error("Erro durante o carregamento!");
-            }
+export default function GetInfo(): JSX.Element {
+  const [res, setRes] = useState<resprop | null>(null);
 
-            const data = await response.json();
-            setRes(data);
 
-        } catch(error) {
-            console.log("Erro durante a requisição!", error);
+  useEffect(() => {
+    async function fetchMain() {
+      try {
+        const response = await fetch("https://apimeioambiente.onrender.com");
+        if (!response.ok) {
+          throw new Error("Erro durante o carregamento!");
         }
+        const data = await response.json();
+        setRes(data);
+      } catch (error) {
+        console.log("Erro durante a requisição!", error);
+      }
     }
 
     fetchMain();
-},[]);
+  }, []);
 
-    console.log(res)
-    return <div className="flex flex-col justify-center items-center">
-        <ul className="flex flex-wrap gap-2">
-            {res.map((res)=>(
-                <li className="bg-white text-black p-4 rounded shadow mb-4">
-                    <h2 className="font-bold">{res.info}</h2>
-                </li>
-            ))}
-        </ul>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1>Informações da API</h1>
+      <ul className="bg-white text-black p-4 rounded shadow mb-4 flex flex-wrap gap-2">
+        {res?.Main?.Importancia && (
+            <li>
+                <h2 className="font-bold">Importância</h2>
+                <p>{res.Main.Importancia}</p>
+            </li>
+        )}
+        {res?.Main?.Dever && (
+        <li>
+            <h2 className="font-bold">Dever</h2>
+            <p>{res.Main.Dever}</p>
+        </li>
+        )}
+      </ul>
     </div>
+  );
 }
