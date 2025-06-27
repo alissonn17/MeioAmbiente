e39f1cca28e02dd014fb0640ddfcff7a342a9b3e
Alisson <llielzinh@gmail.com>
2025-06-26 21:01:22 -0300

    :recycle: getinfo

--- src/app/components/Getinfo.tsx
+++ src/app/components/Getinfo.tsx
@@ -1,20 +1,15 @@
 "use client"
 import { JSX, useEffect, useState } from "react"
 
-interface resprop {
+interface mainprop {
   Main: {
     Importancia: string;
     Dever: string;
   };
 }
 
-/**
- * Get info from main in API-Meio Ambiente.
- */
-
 export default function GetInfo(): JSX.Element {
-  const [res, setRes] = useState<resprop | null>(null);
-
+  const [main, setMain] = useState<mainprop>(Object);
 
   useEffect(() => {
     async function fetchMain() {
@@ -24,7 +19,7 @@ export default function GetInfo(): JSX.Element {
           throw new Error("Erro durante o carregamento!");
         }
         const data = await response.json();
-        setRes(data);
+        setMain(data);
       } catch (error) {
         console.log("Erro durante a requisição!", error);
       }
@@ -33,22 +28,16 @@ export default function GetInfo(): JSX.Element {
     fetchMain();
   }, []);
 
+  console.log(typeof(main))
   return (
     <div className="flex flex-col justify-center items-center">
       <h1>Informações da API</h1>
       <ul className="bg-white text-black p-4 rounded shadow mb-4 flex flex-wrap gap-2">
-        {res?.Main?.Importancia && (
-            <li>
-                <h2 className="font-bold">Importância</h2>
-                <p>{res.Main.Importancia}</p>
-            </li>
-        )}
-        {res?.Main?.Dever && (
-        <li>
-            <h2 className="font-bold">Dever</h2>
-            <p>{res.Main.Dever}</p>
-        </li>
-        )}
+          <li>
+            <h2 className="font-bold">Info</h2>
+            <p>Importancia: {main.Main.Importancia}</p>
+            <p>Dever: {main.Main.Dever}</p>
+          </li>
       </ul>
     </div>
   );
